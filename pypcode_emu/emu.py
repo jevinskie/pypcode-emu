05daabb703c51d6e5d81cefc7fb660fb26088e72
Jevin Sweval <jevinsweval@gmail.com>
2022-02-28 13:33:09 -0800

    wip

---
+++ pypcode_emu/emu.py
@@ -0,0 +1,44 @@
+import mmap
+
+from elftools.elf.elffile import ELFFile
+from pypcode import Arch, Context, PcodePrettyPrinter
+
+
+class PCodeEmu:
+    def __init__(self, spec: str, base: int = 0):
+        arch, endianness, bitness, _ = spec.split(":")
+        assert bitness == "32"
+        self.base = base
+        self.ram = mmap.mmap(-1, 0xFFFF_FFFF)
+        self.ctx = Context(spec)
+
+    def translate(self, addr: int):
+        res = self.ctx.translate(self.ram[addr : addr + 4], self.base, 1)
+        print(res)
+
+    def memcpy(self, addr: int, buf: bytes) -> None:
+        self.ram[addr : addr + len(buf)] = buf
+
+
+class ELFPCodeEmu(PCodeEmu):
+    def __init__(self, elf_path: str):
+        self.elf = ELFFile(open(elf_path, "rb"))
+        machine = {
+            "EM_MICROBLAZE": "mb",
+        }[self.elf.header.e_machine]
+        endianness = {
+            "ELFDATA2MSB": "BE",
+            "ELFDATA2LSB": "LE",
+        }[self.elf.header.e_ident["EI_DATA"]]
+        bitness = {
+            "ELFCLASS32": "32",
+            "ELFCLASS64": "64",
+        }[self.elf.header.e_ident["EI_CLASS"]]
+        super().__init__(f"{machine}:{endianness}:{bitness}:default")
+        for seg_idx in range(self.elf.num_segments()):
+            seg = self.elf.get_segment(seg_idx)
+            if seg.header.p_type != "PT_LOAD":
+                continue
+            self.ram[
+                seg.header.p_vaddr : seg.header.p_vaddr + seg.header.p_filesz
+            ] = seg.data()
---
+++ pypcode_emu/tools/__init__.py
@@ -0,0 +1,1 @@
+from . import pypcode_emu
