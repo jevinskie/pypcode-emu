1b28b5166e61387b7627e4a23555cd8a6e669374
Jevin Sweval <jevinsweval@gmail.com>
2022-02-28 15:39:22 -0800

    wip

--- pypcode_emu/emu.py
+++ pypcode_emu/emu.py
@@ -1,43 +1,88 @@
 import mmap
+import struct
 
+import untangle
 from elftools.elf.elffile import ELFFile
 from pypcode import Arch, Context, PcodePrettyPrinter
 
-
-class VarNode:
-    def __init__(self):
-        pass
-
-
-class Instr:
-    def __init__(self):
-        pass
+from pypcode_emu.utils import *
 
 
 class PCodeEmu:
-    def __init__(self, spec: str, base: int = 0):
+    def __init__(self, spec: str, entry: int = 0):
         arch, endianness, bitness, _ = spec.split(":")
         assert bitness == "32"
-        self.base = base
-        self.ram = mmap.mmap(-1, 0xFFFF_FFFF)
         langs = {l.id: l for arch in Arch.enumerate() for l in arch.languages}
         self.ctx = Context(langs[spec])
+        self.entry = entry
+        self.sla = untangle.parse(self.ctx.lang.slafile_path)
+        self.ram = mmap.mmap(-1, 0xFFFF_FFFF)
+        self.register = mmap.mmap(-1, 0x1000)
+        self.spaces = {
+            "ram": self.ram,
+            "register": self.register,
+        }
+        (
+            self.pc_space,
+            self.pc_off,
+            self.pc_getter,
+            self.pc_setter,
+        ) = self.get_varnode_sym_info("pc")
+        self.pc = property(self.pc_getter, self.pc_setter)
+        self.pc = self.entry
+        self.inst_cache = {}
+
+    def get_varnode_sym_info(self, name: str):
+        sym = first_where_key_is(self.sla.sleigh.symbol_table.varnode_sym, "name", name)
+        assert sym is not None
+        sz = int(sym["size"])
+        space = first_where_key_is(
+            self.sla.sleigh.spaces.space, "name", self.sla.spacessym["space"]
+        )
+        bigendian = space["bigendian"] == "true"
+        space_buf = self.spaces[sym["space"]]
+        off = int(sym["offset"], 16)
+
+        struct_fmt = (">" if bigendian else "<") + {
+            1: "B",
+            2: "H",
+            4: "I",
+            8: "Q",
+        }[sz]
+        unpack_from = struct.Struct(struct_fmt).unpack_from
+        pack_into = struct.Struct(struct_fmt).pack_into
+
+        def getter() -> int:
+            return unpack_from(space_buf, off)[0]
+
+        def setter(val: int) -> None:
+            pack_into(space_buf, off, val)
+
+        return space, off, getter, setter
 
     def translate(self, addr: int):
-        res = self.ctx.translate(self.ram[addr : addr + 4], self.base, 1)
-        for insn in res.instructions:
-            print("-" * 80)
-            print(
-                "%08x/%d: %s %s"
-                % (insn.address.offset, insn.length, insn.asm_mnem, insn.asm_body)
-            )
-            print("-" * 80)
-            for op in insn.ops:
-                print("%3d: %s" % (op.seq.uniq, str(op)))
-            print("")
+        if addr in self.inst_cache:
+            return self.inst_cache[addr]
+        res = self.ctx.translate(self.ram[addr : addr + 4], addr, 1)
+        assert len(res.instructions) == 1
+        insn = res.instructions[0]
+        self.inst_cache[addr] = insn
+        return insn
 
     def memcpy(self, addr: int, buf: bytes) -> None:
         self.ram[addr : addr + len(buf)] = buf
+
+    @staticmethod
+    def dump_instr(insn):
+        print("-" * 80)
+        print(
+            "%08x/%d: %s %s"
+            % (insn.address.offset, insn.length, insn.asm_mnem, insn.asm_body)
+        )
+        print("-" * 80)
+        for op in insn.ops:
+            print("%3d: %s" % (op.seq.uniq, str(op)))
+        print("")
 
 
 class ELFPCodeEmu(PCodeEmu):
@@ -59,6 +104,4 @@ class ELFPCodeEmu(PCodeEmu):
             seg = self.elf.get_segment(seg_idx)
             if seg.header.p_type != "PT_LOAD":
                 continue
-            self.ram[
-                seg.header.p_vaddr : seg.header.p_vaddr + seg.header.p_filesz
-            ] = seg.data()
+            self.memcpy(seg.header.p_vaddr, seg.data())
---
+++ pypcode_emu/utils.py
@@ -0,0 +1,12 @@
+def first(iterable, default=None):
+    for item in iterable:
+        return item
+    return default
+
+
+def first_where(iterable, pred, default=None):
+    return first((x for x in iterable if pred(x)), default=default)
+
+
+def first_where_key_is(iterable, key, val, default=None):
+    return first_where(iterable, lambda x: x[key] == val, default=default)
