2e0ce36f04d8524c6b17e5b8ba1c0d0f924563a2
Jevin Sweval <jevinsweval@gmail.com>
2022-03-15 07:45:08 -0700

    no need to check for delay slots by hand

--- scripts/fact_dump.py
+++ scripts/fact_dump.py
@@ -9,19 +9,21 @@ fact_elf_bin = Path(__file__).parent.parent / "fact"
 emu = ELFPCodeEmu(fact_elf_bin)
 
 bb_addrs = (
-    (0x100000A0, True),
-    (0x100000C8, True),
-    (0x100000D8, True),
-    (0x100000EC, True),
+    0x100000A0,
+    0x100000C8,
+    0x100000D8,
+    0x100000EC,
 )
 
-for bb_addr, has_delay_slot in bb_addrs:
+for bb_addr in bb_addrs:
     for instr in emu.translate(bb_addr):
         emu.dump(instr)
-    if has_delay_slot:
-        instr = emu.translate(
-            instr.address.offset + instr.length, max_bytes=instr.length_delay
-        )[0]
-        emu.dump(instr)
+    else:
+        if instr.length_delay:
+            delay_instrs = emu.translate(
+                instr.address.offset + instr.length, max_bytes=instr.length_delay
+            )
+            for delay_instr in delay_instrs:
+                emu.dump(delay_instr)
 
     print("\n\n\n\n")
