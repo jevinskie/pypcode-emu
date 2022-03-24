884f3372cb84608f683ff6595db91f4125dbf4ff
Jevin Sweval <jevinsweval@gmail.com>
2022-03-24 15:28:02 -0700

    hsv_to_rgb C++

--- pypcode_emu/native/lifted.cpp
+++ pypcode_emu/native/lifted.cpp
@@ -43,3 +43,34 @@ void op_cb(uptr bb, uptr pc, uint32_t op_idx, uint32_t opc, const char *desc) {
           format(fg(color::lawn_green), "{:#010x}", pc), format(fg(color::red), "{:2d}", op_idx),
           desc);
 }
+
+// https://gist.github.com/ditServices/ba3ebabab499afd1056daf828225247f
+
+using rgb_t = struct { double r, g, b; };
+
+using hsv_t = struct { double h, s, v; };
+
+rgb_t hsv_to_rgb(hsv_t hsv) {
+    if (hsv.s == 0.0) {
+        return {hsv.v, hsv.v, hsv.v};
+    }
+    int i    = hsv.h * 6.0;
+    double f = (hsv.h * 6.0) - i;
+    double p = hsv.v * (1.0 - hsv.s);
+    double q = hsv.v * (1.0 - hsv.s * f);
+    double t = hsv.v * (1.0 - hsv.s * (1.0 - f));
+
+    rgb_t rgb = {0, 0, 0};
+
+    // clang-format off
+    switch (i % 6) {
+        case 0: rgb.r = hsv.v; rgb.g = t; rgb.b = p; break;
+        case 1: rgb.r = q; rgb.g = hsv.v; rgb.b = p; break;
+        case 2: rgb.r = p; rgb.g = hsv.v; rgb.b = t; break;
+        case 3: rgb.r = p; rgb.g = q; rgb.b = hsv.v; break;
+        case 4: rgb.r = t; rgb.g = p; rgb.b = hsv.v; break;
+        case 5: rgb.r = hsv.v; rgb.g = p; rgb.b = q; break;
+    }
+    // clang-format on
+    return rgb;
+}
